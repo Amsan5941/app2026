23ee71c461c0eaca4a02e74886d8fd8e68c03812
Amsan <amsan5941@gmail.com>
2026-03-03 14:58:05 -0500

    changes to deploy to app

--- app.config.js
+++ app.config.js
@@ -6,6 +6,7 @@ module.exports = ({ config }) => {
   return {
     ...config,
     extra: {
+      ...config.extra,
       EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
       EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       EXPO_PUBLIC_SENTRY_DSN: process.env.EXPO_PUBLIC_SENTRY_DSN,
